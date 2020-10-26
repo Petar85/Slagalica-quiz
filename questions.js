37112bb3b9188b3e0302cf402fffc7b90cc8b4a0
Peter Pan <petarzivkovic85@gmail.com>
2020-10-26 11:48:40 -0700

    html done

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: 'What is the capital of US?',
+        answers: ['Los Angeles', 'Washington D.C.', 'New York', 'Seattle'],
+        correct: 'Washington D.C.'
+    },
+    {
+        question: 'What is the capital of Canada?',
+        answers: ['Ottawa', 'Montreal', 'Victoria', 'Quebec'],
+        correct: 'Ottawa'
+    },
+    {
+        question: 'What is the capital of Germany?',
+        answers: ['Berlin', 'Bon', 'Dortmund', 'Hamburgh'],
+        correct: 'Berlin'
+    },
+    {
+        question: 'What is the capital of France?',
+        answers: ['Paris', 'Strasbourg', 'Tolouse', 'Nantes'],
+        correct: 'Paris'
+    },
+    {
+        question: 'What is the capital of Italy?',
+        answers: ['Rome', 'Naples', 'Torino', 'Genoa'],
+        correct: 'Rome'
+    }
+];
